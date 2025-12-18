1d800b282ece050836d08f65357bf5f3ac33409a
St1nce <99748650+St1nce@users.noreply.github.com>
2025-12-18 12:56:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+})
+
+const nextEl = document.getElementsByClassName('swiper-button-next')[0]
+const prevEl = document.getElementsByClassName('swiper-button-prev')[0]
+
